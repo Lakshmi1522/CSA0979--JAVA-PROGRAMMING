55a91b0655a271d8c84d006e587f3246302103ac
Lakshmi1522 <112838460+Lakshmi1522@users.noreply.github.com>
2023-05-05 13:14:11 +0530

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Grade
+{
+public static void main(String args[] )
+{
+System.out.println("Enter the marks in python:");
+System.out.println("Enter the marks in c programming:");
+System.out.println("Enter the marks in mathematics:");
+System.out.println("Enter the marks in physics:");
+System.out.println("Enter the marks in chemistry");
+System.out.println("Enter the marks in professional ethics:");
+{
+Scannner sc = new Scanner(System.in);
+{
+if marks > 75%
+System.out.println("Grade is distinction");
+}
+{
+if marks  >60% & <70%
+System.out.println("Grade is first division");
+}
+{
+if marks >50% & < 60%
+System.out.println("Grade is second division");
+}
+{
+if marks  >40% & < 50%
+System.out.println("Grade is third division");
+}
+{
+if else marks <50%
+System.out.println("Grade is Fail");
+}
+total = 551 = "aggregate" + "class"
+}
+}
+}
+
+
+
