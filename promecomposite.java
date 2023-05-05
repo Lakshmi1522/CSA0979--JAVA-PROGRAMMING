5aa1cff62a43f9d60155318470809ce46c7aa1e5
Lakshmi1522 <112838460+Lakshmi1522@users.noreply.github.com>
2023-05-05 13:15:17 +0530

    Add files via upload

---
+++ promecomposite.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class primecomposite{
+public static void main(String[] args)
+{
+Scanner sc=new Sacnner(System.in);
+System.out.println("enter the number:");
+int n=sc.next();
+int count=0;
+for(int i=1;i<=n;i++)
+{
+if(n%i==0)
+{
+
+
